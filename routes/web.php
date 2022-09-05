82e6bd6e381af7ffb4b93db7b1ebdb9a8017f894
rizkyauliaf <rizkyauliaf7@gmail.com>
2022-09-05 22:53:06 +0700

    Membuat PageController

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('posts', [PostController::class, 'index']);
+
+Route::get('/', function () {
+    echo "Selamat Datang";
+});
+
+Route::get('/about', function () {
+    echo "2241727037, Rizky Aulia Faridatuzzuhro kelas Web B";
+});
+
+Route::get('/articles/{id}', function ($id) {
+    echo "Halaman Artikel dengan ID" . $id;
+});
